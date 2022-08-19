221fc92e3ac7ae250d9a8f1c18b19fe12400476e
Georgiana <georgianaemiliou@gmail.com>
2022-08-19 10:10:30 +0100

    Initial commit

---
+++ src/main/java/com/sparta/ge/ThreadBasics.java
@@ -0,0 +1,15 @@
+package com.sparta.ge;
+
+public class ThreadBasics implements Runnable{ //Separates the functionality from the thread
+
+    @Override
+    public void run(){
+        System.out.println("I am printing from a thread!");
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ThreadBasics());
+        Thread thread2 = new Thread(() -> System.out.println("Do something"));
+        thread1.start();
+    }
+}
---
+++ src/main/java/com/sparta/ge/Util.java
@@ -0,0 +1,7 @@
+package com.sparta.ge;
+
+public class Util {
+    public static String getThreadName(){
+        return Thread.currentThread().getName();
+    }
+}
